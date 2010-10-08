9e2ebc7839a02f090707c7bae3ff54371c096020
Jared Faris <jfaris@gmail.com>
2010-10-08 19:20:25 -0400

    Fixed a typo

--- numberGenerator.js
+++ numberGenerator.js
@@ -9,3 +9,16 @@ function GenerateTestArray (length) {
   return numbers;
 }
 
+function GenerateComplexTestArray (length) {
+
+  var numbers = new Array();
+
+  for( var i = 0; i < length; i++ )
+  {
+    var subArray = { childName: "String"+i+"2", childValue: (i * 10) };
+    numbers[i] = { name: "String"+i, value: i};
+  }
+
+  return numbers;
+}
+
